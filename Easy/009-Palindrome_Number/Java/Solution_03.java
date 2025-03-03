3e40d34828eb28b4a3d49983ef960b46c3b9c1bd
Emre Önsür <emreonsur@gmail.com>
2025-03-03 13:52:23 +0100

    Create Solution_03.java

---
+++ Easy/009-Palindrome_Number/Java/Solution_03.java
@@ -0,0 +1,27 @@
+// Solution inspired by [Gourab/https://leetcode.com/problems/palindrome-number/solutions/3213890/fastest-java-solution/]
+
+class Solution {
+    public boolean isPalindrome(int x) {
+        if (x < 0 || x != 0 && x % 10 == 0)
+            return false;
+
+        if (x < 10)
+            return true;
+
+        return x == NumberUtil.reverse(x);
+    }
+}
+
+class NumberUtil {
+    public static int reverse(int x)
+    {
+        int rev = 0;
+
+        while (x != 0) {
+            rev = rev * 10 + x % 10;
+            x /= 10;
+        }
+
+        return rev;
+    }
+}
